a6fef45d54de6250a6aa1b13ed7f556590ce4303
tasatkoEV <tasatko@gmail.com>
2014-04-21 01:33:59 +0300

    add qEngine base class

---
+++ Engines/qEngine.php
@@ -0,0 +1,22 @@
+<?php
+
+class qEngine {
+    private $statusOn = "false";
+    private $clockWise = "true";
+    
+    public function getStatusOn() {
+        return $this->statusOn;
+    }
+    
+    public function setStatusOn($status = false) {
+        $this->statusOn = $status;
+    }
+    
+    public function getClockWise() {
+        return $this->clockWise;
+    }
+    
+    public function setClockWise($status = true) {
+        $this->clockWise = $status;
+    }
+}
